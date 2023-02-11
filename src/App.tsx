11196d8d551d8fcf661618a54e42533d23384318
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-02-10 21:55:09 -0300

    style: finish App

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import { Ticket } from "./components/Ticket/Ticket";
 
 function App() {
 	return (
-		<Flex direction="column" justify="center" align="center" gap="2rem">
+		<Flex
+			direction="column"
+			justify="center"
+			align="center"
+			gap="2rem"
+			filter="drop-shadow(0px 4px 24px rgba(0, 0, 0, 0.25))"
+		>
 			<Heading as="h1" color="#FFFFFF" fontSize="2rem">
 				Boarding pass
 			</Heading>
